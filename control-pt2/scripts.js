a52e2b1ff6d918ea58fb5744e080f6969553495b
osornioariana <osornioa@vcu.edu>
2022-11-01 17:43:20 -0400

    Javascript - Control

---
+++ control-pt2/scripts.js
@@ -0,0 +1,31 @@
+
+$("#reveal-btn").click(function(){
+
+    $(".button").css('opacity', '1');
+    $(this).hide();
+
+});
+
+
+$("#reveal-btn2").click(function(){
+
+    $(".button").css('opacity', '1');
+    $(this).hide();
+
+});
+
+
+$("#reveal-btn3").click(function(){
+
+    $(".button").css('opacity', '1');
+    $(this).hide();
+
+});
+
+
+$("#reveal-btn4").click(function(){
+
+    $(".button").css('opacity', '1');
+    $(this).hide();
+
+});
